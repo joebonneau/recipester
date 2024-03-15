f71929a445e5918632769244c6cf1d0c2e06902b
Joe Bonneau <joebonneau@gmail.com>
2024-03-15 16:37:56 -0700

    wip: more progress and some new models

--- recipester/recipester/models.py
+++ recipester/recipester/models.py
@@ -23,10 +23,24 @@ class User(AbstractUser): ...
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     quantity = models.CharField(max_length=200)
+    unit = models.ManyToManyField("Unit")
+    quantity_range = models.ForeignKey("QuantityRange", on_delete=models.CASCADE)
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.quantity} {self.name}"
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class QuantityRange(models.Model):
+    min = models.FloatField()
+    max = models.FloatField()
 
 
 class Note(models.Model):
--- scratch.py
+++ scratch.py
@@ -1,7 +1,11 @@
+import re
 from dataclasses import dataclass
 
 import requests
-from bs4 import BeautifulSoup, NavigableString, Tag
+from bs4 import BeautifulSoup, NavigableString, Tag, ResultSet
+
+from recipester.recipester.models import Ingredient, Recipe, Unit
+
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
@@ -59,6 +63,10 @@ def transform_quantity(quantity: str) -> float | QuantityRange | None:
     if "-" in quantity:
         min, max = quantity.split("-")
         return QuantityRange(float(min), float(max))
+    # NOTE: This is not the same character as the last conditonal
+    if "–" in quantity:
+        min, max = quantity.split("–")
+        return QuantityRange(float(min), float(max))
     for key, value in QUANTITY_TRANSLATIONS.items():
         if key in quantity:
             # remove string fraction from quantity str, then add
@@ -70,25 +78,143 @@ def transform_unit(unit: str) -> str:
     return UNIT_TRANSLATIONS.get(unit, unit)
 
 
-def transform_ingredient(ingredient: str) -> tuple[str, IngredientOptions | str]:
+def transform_ingredient(
+    ingredient: str, qty: float | QuantityRange | None
+) -> tuple[float | QuantityRange | None, str | None, IngredientOptions | str]:
     # TODO: handle adjectives such as "cubed" or "diced" such that each option has the adjective
-    unit = ""
+    unit = None
+    subs = ""
     for key in UNIT_TRANSLATIONS:
         if key in ingredient:
+            subs = key
             unit = UNIT_TRANSLATIONS[key]
             break
     if unit and ingredient.startswith(unit):
-        ingredient = ingredient.replace(unit, "").strip()
+        ingredient = ingredient.replace(subs, "").strip()
     options = []
-    # TODO: properly handle output of this split, as the options might be full entries themselves
-    # e.g. "1 tsp Diamond Crystal salt or 1/4 tsp Morton salt" currently parses to "1 tsp Diamond Crystal salt" and "1/4 tsp Morton salt"
-    # where the first entry has entries in both the quantity and unit fields, and the second is all in the "ingredient"
-    # Might want to turn this all into a big class that does this recursively and outputs the final results
-    if " or " in ingredient:
-        options = ingredient.split(" or ")
-    if " and/or " in ingredient:
-        options = ingredient.split(" and/or ")
-    return unit, IngredientOptions(options=options) if options else ingredient
+    for separator in [" or ", " and/or "]:
+        if separator in ingredient:
+            options = ingredient.split(separator)
+            if re.match(options[1], r"^\d+"):
+                # add the qty to the first option if the second option also has a qty
+                options[0] = f"{qty} {options[0]}"
+                qty = qty
+            else:
+                qty = None
+    return qty, unit, IngredientOptions(options=options) if options else ingredient
+
+
+def parse_nyt_cooking(ingredients_li: Tag):
+    quantities = []
+    units = []
+    items = []
+    for li in ingredients_li:
+        spans = li.find_all("span")
+        if len(spans) == 2:
+            qty = transform_quantity(spans[0].get_text().strip())
+            qty, unit, item = transform_ingredient(spans[1].get_text().strip(), qty)
+            quantities.append(qty)
+            units.append(unit)
+            items.append(item)
+        else:
+            qty, unit, item = transform_ingredient(li.get_text().strip(), None)
+            quantities.append(qty)
+            units.append(unit)
+            items.append(item)
+    return quantities, units, items
+
+
+def parse_delish(ingredients_li: ResultSet):
+    quantities = []
+    units = []
+    items = []
+    for li in ingredients_li:
+        strongs = li.find_all("strong")
+        ps = li.find_all("p")
+        if strongs:
+            qty = transform_quantity(strongs[0].get_text().strip())
+            units.append(
+                (
+                    transform_unit(strongs[1].get_text().strip())
+                    if len(strongs) > 1
+                    else ""
+                )
+            )
+            if isinstance(qty, str):
+                qty = float(qty)
+            qty, _, item = transform_ingredient(ps[0].get_text().strip(), qty)
+            quantities.append(qty)
+            items.append(item)
+    return quantities, units, items
+
+
+def parse_half_baked_harvest_love_and_lemons(ingredients_li: ResultSet):
+    units = []
+    quantities = []
+    items = []
+    for li in ingredients_li:
+        qty_li = li.find(
+            "span",
+            class_=lambda x: "amount" in x if x is not None else False,
+        )
+        qty = (
+            transform_quantity(qty_li.get_text().strip())
+            if qty_li is not None
+            else None
+        )
+        unit = li.find("span", class_=lambda x: "unit" in x if x is not None else False)
+        units.append(
+            transform_unit(unit.get_text().strip()) if unit is not None else ""
+        )
+        item = li.find("span", class_=lambda x: "name" in x if x is not None else False)
+        qty, _, item = transform_ingredient(item.get_text().strip(), qty)
+        quantities.append(qty)
+        items.append(item)
+    return quantities, units, items
+
+
+def parse_bon_appetit(ingredients_div: Tag | NavigableString | None):
+    items_units = [
+        div.get_text().strip() for div in ingredients_div.div.find_all("div")
+    ]
+    items = []
+    units = []
+    quantities = [
+        transform_quantity(div.get_text().strip())
+        for div in ingredients_div.div.find_all("p")
+        if isinstance(ingredients_div, Tag)
+    ]
+    for i in range(len(quantities)):
+        qty = quantities[i]
+        item_unit = items_units[i]
+        qty, unit, item = transform_ingredient(item_unit, qty)
+        units.append(transform_unit(unit))
+        items.append(item)
+        quantities[i] = qty
+    return quantities, units, items
+
+
+def create_entities(
+    title: str,
+    description: str,
+    site_name: str,
+    quantities: list[str | QuantityRange | None],
+    units: list[str | None],
+    ingredients: list[str | IngredientOptions],
+):
+    for i in range(len(ingredients)):
+        if isinstance(ingredients[i], IngredientOptions):
+            Ingredient.objects.create(name=" or ".join(ingredients[i].options))
+        else:
+            Ingredient.objects.create(name=ingredient[i])
+
+        if not Unit.objects.get(name=units[i]):
+            unit = Unit.objects.create(name=units[i])
+
+    # Create Recipe entity
+    recipe = Recipe.objects.create(
+        title=title, description=description, sie_name=site_name
+    )
 
 
 def parse_url(response):
@@ -114,79 +240,18 @@ def parse_url(response):
         items = []
         match site_name:
             case "NYT Cooking":
-                for li in ingredients_li:
-                    spans = li.find_all("span")
-                    if len(spans) == 2:
-                        quantities.append(
-                            transform_quantity(spans[0].get_text().strip())
-                        )
-                        unit, item = transform_ingredient(spans[1].get_text().strip())
-                        units.append(unit)
-                        items.append(item)
-                    else:
-                        quantities.append("")
-                        unit, item = transform_ingredient(li.get_text().strip())
-                        units.append(unit)
-                        items.append(item)
+                quantities, units, items = parse_nyt_cooking(ingredients_li)
             case "Delish":
-                for li in ingredients_li:
-                    strongs = li.find_all("strong")
-                    ps = li.find_all("p")
-                    if strongs:
-                        quantities.append(
-                            transform_quantity(strongs[0].get_text().strip())
-                        )
-                        units.append(
-                            (
-                                transform_unit(strongs[1].get_text().strip())
-                                if len(strongs) > 1
-                                else ""
-                            )
-                        )
-                        _, item = transform_ingredient(ps[0].get_text().strip())
-                        items.append(item)
+                quantities, units, items = parse_delish(ingredients_li)
             case "Half Baked Harvest" | "Love and Lemons":
-                for li in ingredients_li:
-                    qty = li.find(
-                        "span",
-                        class_=lambda x: "amount" in x if x is not None else False,
-                    )
-                    quantities.append(
-                        transform_quantity(qty.get_text().strip())
-                        if qty is not None
-                        else ""
-                    )
-                    unit = li.find(
-                        "span", class_=lambda x: "unit" in x if x is not None else False
-                    )
-                    units.append(
-                        transform_unit(unit.get_text().strip())
-                        if unit is not None
-                        else ""
-                    )
-                    item = li.find(
-                        "span", class_=lambda x: "name" in x if x is not None else False
-                    )
-                    _, item = transform_ingredient(item.get_text().strip())
-                    items.append(item)
+                quantities, units, items = parse_half_baked_harvest_love_and_lemons(
+                    ingredients_li
+                )
             case _:
                 pass
     else:
-        # Bon Appetit
         ingredients_div = soup.find("div", attrs={"data-testid": "IngredientList"})
-        items_units = [
-            div.get_text().strip() for div in ingredients_div.div.find_all("div")
-        ]
-        items = []
-        units = []
-        for iu in items_units:
-            unit, item = transform_ingredient(iu)
-            units.append(unit)
-            items.append(item)
-        quantities = [
-            transform_quantity(div.get_text().strip())
-            for div in ingredients_div.div.find_all("p")
-        ]
+        quantities, units, items = parse_bon_appetit(ingredients_div)
     description_obj: Tag | NavigableString | None = soup.find(
         "meta", attrs={"name": "description"}
     )
@@ -195,6 +260,7 @@ def parse_url(response):
         description = description_obj.get("content", "")
     print("title: ", title, "description: ", description)
     print(quantities, units, items)
+    create_entities(title, description, quantities, units, items)
 
 
 if __name__ == "__main__":
@@ -212,7 +278,11 @@ if __name__ == "__main__":
     # response = requests.get(
     #     "https://www.loveandlemons.com/honey-mustard-dressing-recipe/", headers=headers
     # )
+    # response = requests.get(
+    #     "https://www.bonappetit.com/recipe/sesame-tofu-with-broccoli", headers=headers
+    # )
     response = requests.get(
-        "https://www.bonappetit.com/recipe/sesame-tofu-with-broccoli", headers=headers
+        "https://www.bonappetit.com/recipe/sheet-pan-halloumi-avocado-citrus",
+        headers=headers,
     )
     parse_url(response)
