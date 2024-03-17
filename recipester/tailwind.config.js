87d07b2838f7ac4026b22a911ef6c911728c0d58
Joe Bonneau <joebonneau@gmail.com>
2024-03-16 18:06:13 -0700

    add daisyui and typography tailwind plugins; revamp top nav

--- recipester/tailwind.config.js
+++ recipester/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./recipester/templates/**/*.dhtml", "./recipester/*.py"],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
 };
